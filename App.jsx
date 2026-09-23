8c2dc4f9b6a3e4fc294a44322c76bf16fd6f37c9
abdulsaiedmaniyar25-pixel <abdulsaied.maniyar25@vupune.ac.in>
2026-09-23 15:41:12 +0530

    Create initial App component for landing page

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination
+        for beautiful and healthy plants.
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
