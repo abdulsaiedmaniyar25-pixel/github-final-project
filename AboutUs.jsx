323379379d6a86cd3fc0949b22287c08141de1e6
abdulsaiedmaniyar25-pixel <abdulsaied.maniyar25@vupune.ac.in>
2026-09-23 15:36:36 +0530

    Add AboutUs component with company information

---
+++ AboutUs.jsx
@@ -0,0 +1,21 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+
+      <h2>Our Company</h2>
+
+      <p>
+        We are a company dedicated to providing innovative and
+        reliable solutions to our customers.
+      </p>
+
+      <p>
+        Our goal is to deliver high-quality products and services
+        while creating a great experience for our customers.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
